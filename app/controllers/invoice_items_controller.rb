e366e32b75c451f71b6e3059752e84748dbabafb
djcuze <natejacobymusic@gmail.com>
2018-01-14 00:04:15 +1000

    Can create InventoryItem within InvoiceItem Form

--- app/controllers/invoice_items_controller.rb
+++ app/controllers/invoice_items_controller.rb
@@ -12,8 +12,8 @@ class InvoiceItemsController < ApplicationController
   def create
     @invoice_item = InvoiceItem.new(invoice_item_params)
     respond_to do |format|
-      if @invoice_item.save
-        format.html { redirect_to @invoice_item, notice: 'Invoice Item was successfully created'}
+      if @invoice_item.save!
+        format.html { redirect_to @invoice_item, notice: 'Invoice Item was successfully created' }
       else
         format.html { render :new }
       end
@@ -23,7 +23,7 @@ class InvoiceItemsController < ApplicationController
   def destroy
     @invoice_item.destroy
     respond_to do |format|
-      format.html { redirect_to invoice_items_url, notice: 'Invoice Item was successfully destroyed.'}
+      format.html { redirect_to invoice_items_url, notice: 'Invoice Item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -35,6 +35,6 @@ class InvoiceItemsController < ApplicationController
   end
 
   def invoice_item_params
-    params.require(:invoice_item).permit(:cost, :quantity, :invoice_id, :invoice_item_id)
+    params.require(:invoice_item).permit(:cost, :quantity, :invoice_id, :inventory_item_id, inventory_item_attributes: [:description, :quantity, :_destroy, :id])
   end
 end
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,5 +1,6 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :inventory_item
-  validates_presence_of :quantity, :cost, :inventory_item_id, :invoice_id
+  validates_presence_of :quantity, :cost, :inventory_item, :invoice_id
+  accepts_nested_attributes_for :inventory_item, reject_if: :all_blank, allow_destroy: true
 end
