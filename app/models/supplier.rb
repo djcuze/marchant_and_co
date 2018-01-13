44a97d562e3c30ba6b66a73a4313965a5d306eb2
djcuze <natejacobymusic@gmail.com>
2018-01-13 15:13:46 +1000

    Supplier model added. supplier_spec written with very simple validation

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,2 +1,3 @@
 class Supplier < ApplicationRecord
+  validates_presence_of :name
 end
---
+++ spec/models/supplier_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Supplier, type: :model do
+  subject { described_class.new }
+
+  context 'given a name' do
+    before(:each) { subject.name = 'foo' }
+
+    it 'is valid' do
+      expect(subject).to be_valid
+    end
+  end
+
+  context 'without a name' do
+    it 'is not valid' do
+      expect(subject).not_to be_valid
+    end
+  end
+end
