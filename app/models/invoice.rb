3fd9a2a33543590d73b67dcdc81b3094aa7d2d90
djcuze <natejacobymusic@gmail.com>
2018-01-14 18:30:42 +1000

    Invoice now calculates its total cost

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,4 +2,8 @@ class Invoice < ApplicationRecord
   belongs_to :supplier
   has_many :invoice_items, dependent: :destroy
   accepts_nested_attributes_for :invoice_items, reject_if: :all_blank, allow_destroy: true
+
+  def total
+    @total = invoice_items.sum(&:calculated_total)
+  end
 end
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -5,4 +5,9 @@ class InvoiceItem < ApplicationRecord
   validates :quantity, numericality: { only_integer: true, greater_than: 0 }
   validates :cost, numericality: true
   accepts_nested_attributes_for :inventory_item, reject_if: :all_blank, allow_destroy: true
+  validates_associated :inventory_item
+
+  def calculated_total
+    cost * quantity
+  end
 end
