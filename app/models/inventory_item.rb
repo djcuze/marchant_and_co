5829340286a81ec01e0ee05a2fef404a6a677ee7
djcuze <natejacobymusic@gmail.com>
2018-01-15 16:44:27 +1000

    Invoice views now display number values as currencies

--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -2,7 +2,7 @@ class InventoryItem < ApplicationRecord
   has_many :invoice_items, dependent: :destroy
   belongs_to :category
   validates_presence_of :description
-  validates_uniqueness_of :description, :case_sensitive => false
+  # validates_uniqueness_of :description, :case_sensitive => false
   validates_associated :invoice_items
 
   def quantity
