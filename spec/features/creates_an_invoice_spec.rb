5738cc692953311971734a145c7ff8147bb1f6d7
djcuze <natejacobymusic@gmail.com>
2018-01-15 13:56:18 +1000

    Create Invoice Spec written

---
+++ spec/features/creates_an_invoice_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'Creating an invoice' do
+  let!(:supplier) { FactoryBot.create(:supplier) }
+  let!(:inventory_item) { FactoryBot.build(:inventory_item) }
+
+  feature 'creates an invoice' do
+    before(:each) do
+      create_invoice_with(inventory_item)
+    end
+
+    scenario 'with valid items' do
+      expect(page).to have_content('You have successfully created an invoice!')
+    end
+
+    it 'increases the inventory_item count by 1' do
+      expect { create_invoice_with(inventory_item) }.to change { InventoryItem.count }.by(1)
+    end
+
+    def create_invoice_with(inventory_item)
+      visit new_invoice_path
+      select(supplier.name, :from => 'invoice_supplier_id')
+      fill_in 'Quantity', with: '5'
+      fill_in 'Cost', with: '5'
+      fill_in 'Description', with: inventory_item
+      click_button 'Create Invoice'
+    end
+  end
+end
