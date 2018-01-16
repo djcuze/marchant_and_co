009a51cb22b6ed6722420c9d3c495d74cb53f211
djcuze <natejacobymusic@gmail.com>
2018-01-16 22:59:59 +1000

    Invoice Index page fully styled

--- app/assets/javascripts/invoice.js
+++ app/assets/javascripts/invoice.js
@@ -1,16 +1,17 @@
-$(document).ready(function () {
-    console.log('jQuery loaded')
-});
-
-jQuery(function() {
+jQuery(function () {
     return $('#invoice_invoice_items_attributes_0_inventory_item_attributes_description').autocomplete({
         source: $('#invoice_invoice_items_attributes_0_inventory_item_attributes_description').data('autocomplete-source')
     });
 });
 
-jQuery(function() {
+jQuery(function () {
     var category_name = $('#invoice_invoice_items_attributes_0_inventory_item_attributes_category_name')
     return category_name.autocomplete({
         source: category_name.data('autocomplete-source')
     });
-})
+});
+jQuery(document).ready(function ($) {
+    $(".clickable-row").click(function () {
+        window.location = $(this).data("href");
+    });
+});
